723c9fd2490541af65c051a48c23451d9d58ef1f
AN VAN <vanxuanbinhan01@gmail.com>
2018-05-06 15:48:33 +0700

    add source

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <h1>Register</h1>
+      <input type="text" name="email" placeholder="email" v-model="email">
+      <br>
+      <input type="password" name="password" placeholder="password" v-model="password">
+      <br>
+      <button @click="register">Register</button>
+  </div>
+</template>
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  watch: {
+    email(value) {
+      console.log("email: ", value);
+    }
+  },
+  methods:{
+      register() {
+          console.log('registerd: ', this.email, this.password);
+      }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = "vanxuanbinhan@gmail.com";
+    }, 2000);
+  }
+};
+</script>
